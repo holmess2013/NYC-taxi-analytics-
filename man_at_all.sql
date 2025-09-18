57cc4523e46cacd5f4b026ace0646cfc9a4b39a8
holmess2013 <76541019+holmess2013@users.noreply.github.com>
2025-09-17 20:27:54 -0400

    Add files via upload

---
+++ man_at_all.sql
@@ -0,0 +1,22 @@
+
+SELECT
+    t.PULocationID,
+    pu.Borough   AS pickup_borough,
+    pu.Zone      AS pickup_zone,
+    pu.LocationID AS pickup_locationid,
+
+    t.DOLocationID,
+    d.Borough   AS dropoff_borough,
+    d.Zone      AS dropoff_zone,
+    d.LocationID AS dropoff_locationid,
+
+    t.fare_amount
+FROM trips AS t
+LEFT JOIN locations AS pu
+    ON t.PULocationID = pu.LocationID
+LEFT JOIN locations AS d
+    ON t.DOLocationID = d.LocationID
+WHERE pu.Borough = 'Manhattan'
+   OR d.Borough = 'Manhattan'
+ORDER BY t.tpep_pickup_datetime;
+
